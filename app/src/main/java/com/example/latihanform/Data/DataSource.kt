ec99510a5e46ac83f6fe5b8c607a62d441848ff5
MuhFadila <muh.fadila18@gmail.com>
2023-10-24 19:00:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanform/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.latihanform.Data
+
+import com.example.latihanform.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.Laki,
+        R.string.Perempuan
+    )
+}
